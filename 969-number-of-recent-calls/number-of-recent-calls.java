3cbe6df2f062760a7d60f416938de380f391ad83
Harshit Ghosh <128123681+Harshit-code-tech@users.noreply.github.com>
2024-06-02 11:22:01 +0530

    Time: 56 ms (14.04%) | Memory: 53.5 MB (90.50%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,22 +1,20 @@
 class RecentCounter {
-    ArrayList<Integer> lst;
+    int [] times ;
+    int size = 0;
     public RecentCounter() {
-        lst=new ArrayList<>();
+        this.times = new int[10000];
     }
     
     public int ping(int t) {
-        lst.add(t);
-        int min=t-3000;
-        int max=t;
-        int count=lst.size();
-        for(int i=0;i<lst.size();i++){
-            if(lst.get(i)<min || lst.get(i)>max){
-                count--;
+        times[size++] = t;
+        int count = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (t - times[i] <= 3000) {
+                count++;
             }
+            else break;
         }
-
-        
-        return count;
+        return count ;
     }
 }
 
