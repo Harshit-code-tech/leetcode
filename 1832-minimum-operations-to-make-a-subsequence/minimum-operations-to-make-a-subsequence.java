051c755590658e59155814c5d27455bc1cb8b6f4
Harshit Ghosh <128123681+Harshit-code-tech@users.noreply.github.com>
2024-06-03 07:45:05 +0530

    Time: 124 ms (12.96%) | Memory: 68.1 MB (7.41%) - LeetSync

---
+++ 1832-minimum-operations-to-make-a-subsequence/minimum-operations-to-make-a-subsequence.java
@@ -0,0 +1,22 @@
+public class Solution{ 
+    public int minOperations(int[] target, int[] A) {
+        Map<Integer, Integer> h = new HashMap<>();
+        for (int i = 0; i < target.length; i++) {
+            h.put(target[i], i);
+        }
+        TreeSet<Integer> stack = new TreeSet<>();
+        for (int a : A) {
+            if (!h.containsKey(a)) {
+                continue;
+            }
+            if (stack.isEmpty() || stack.last() < h.get(a)) {
+                stack.add(h.get(a));
+                continue;
+            }
+            int key = stack.ceiling(h.get(a));
+            stack.remove(key);
+            stack.add(h.get(a));
+        }
+        return target.length - stack.size();
+    }
+}
