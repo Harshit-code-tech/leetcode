d72928a2ac5b9c9c55ff19319cd3cda942c3c5c0
Harshit Ghosh <harshitghosh6@gmail.com>
2024-06-09 17:13:34 +0530

    Time: 3 ms (99.31%) | Memory: 45.5 MB (93.66%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,32 +1,15 @@
-public class Solution {
+class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int count = 0;
-        // Use a hash table to store the prefix sum remainders and their frequencies
-        Map<Integer, Integer> remainderCount = new HashMap<>();
-
-        // Initialize remainderCount with 1 for remainder 0 (empty subarray)
-        remainderCount.put(0, 1);
-
-        int sum = 0;
-        for (int num : nums) {
-            // Add current element to the sum
-            sum += num;
-
-            // Handle negative remainders (Java % for negative numbers can be tricky)
-            int remainder = sum % k;
-            if (remainder < 0) {
-                remainder += k; // Make sure remainder is non-negative
-            }
-
-            // Check if a previous subarray with the same remainder exists
-            if (remainderCount.containsKey(remainder)) {
-                count += remainderCount.get(remainder);
-            }
-
-            // Update frequency of the current remainder
-            remainderCount.put(remainder, remainderCount.getOrDefault(remainder, 0) + 1);
+        int[] fre=new int[k];
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=((nums[i]%k)+k)%k;
+            fre[sum%k]++;
         }
-
-        return count;
+        int res=fre[0];
+        for(int i:fre){
+            res+=(i*(i-1))/2;
+        }
+        return res;
     }
 }
