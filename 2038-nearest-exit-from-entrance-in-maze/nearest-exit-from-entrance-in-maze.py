c898b4c05fb430480e5e1758ece3987b82a9cd41
Harshit Ghosh <harshitghosh6@gmail.com>
2024-06-15 09:13:30 +0530

    Time: 625 ms (92.20%) | Memory: 17.1 MB (70.71%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,19 +1,36 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        m,n=entrance; queue=[(m,n)]; ans=0; seen=set(); seen.add((m,n))
-        while queue:
-            for _ in range(len(queue)):
-                x,y=queue.pop(0)
-            
-                for dx,dy in ((-1,0),(0,-1),(0,1),(1,0)):
-                    p=x+dx; q=y+dy
-                
-                    if 0<=p<len(maze) and 0<=q<len(maze[0]) and maze[p][q]=="." and (p,q) not in seen:
-                        queue.append((p,q))
-                        seen.add((p,q))
-                    
-                        if (p==0 or p==len(maze)-1 or q==0 or q==len(maze[0])-1) :
-                            return ans+1      
-            ans+=1
-        return -1
+        rows, cols = len(maze), len(maze[0])
+        dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
         
+        # Mark the entrance as visited since its not a exit.
+        start_row, start_col = entrance
+        maze[start_row][start_col] = "+"
+        
+        # Start BFS from the entrance, and use a queue `queue` to store all 
+        # the cells to be visited.
+        queue = collections.deque()
+        queue.append([start_row, start_col, 0])
+        
+        while queue:
+            curr_row, curr_col, curr_distance = queue.popleft()
+            
+            # For the current cell, check its four neighbor cells.
+            for d in dirs:
+                next_row = curr_row + d[0]
+                next_col = curr_col + d[1]
+                
+                # If there exists an unvisited empty neighbor:
+                if 0 <= next_row < rows and 0 <= next_col < cols \
+                    and maze[next_row][next_col] == ".":
+                    
+                    # If this empty cell is an exit, return distance + 1.
+                    if 0 == next_row or next_row == rows - 1 or 0 == next_col or next_col == cols - 1:
+                        return curr_distance + 1
+                    
+                    # Otherwise, add this cell to 'queue' and mark it as visited.
+                    maze[next_row][next_col] = "+"
+                    queue.append([next_row, next_col, curr_distance + 1])
+            
+        # If we finish iterating without finding an exit, return -1.
+        return -1
