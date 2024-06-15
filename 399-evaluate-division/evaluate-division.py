23f7df1a9268eb36042e0cb0a4fbe42cf61f5bb5
Harshit Ghosh <harshitghosh6@gmail.com>
2024-06-15 09:10:38 +0530

    Time: 44 ms (7.27%) | Memory: 16.4 MB (97.41%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,26 +1,38 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        n = len(equations)
         graph = defaultdict(dict)
-        for idx, (src, dest) in enumerate(equations):
-            graph[src][dest] = values[idx]
-            graph[dest][src] = 1/values[idx]
 
-        def dfs(src, dest, res):
-            if src not in graph or dest not in graph or src in visited:
+        for (A, B), value in zip(equations, values):
+            graph[A][B] = value
+            graph[B][A] = 1.0/value
+
+        def findPath(C, D) -> float:
+            if C not in graph or D not in graph:
                 return -1
-            if src == dest:
-                return res
-            visited.add(src)
-            for nei, val in graph[src].items():
-                temp = dfs(nei, dest, res * val)
-                if temp != -1:
-                    return temp
-            return -1
-        result = []
-        for src, dest in queries:
-            visited = set()
-            val = dfs(src, dest, 1)
-            result.append(val)
             
+            if C == D:
+                return 1
+            
+            stack = [(C, 1)]
+            visited = set()
+
+            while(stack):
+                (nodeLabel, value) = stack.pop()
+                visited.add(nodeLabel)
+
+                if (nodeLabel == D):
+                    return value
+
+                for neighbor in graph[nodeLabel].items():
+                    if (neighbor[0] not in visited):
+                        stack.append((neighbor[0], value * neighbor[1]))
+            
+            return -1
+
+        result = []
+
+        for (C, D) in queries:
+            result.append(findPath(C, D))
+        
         return result
+        
