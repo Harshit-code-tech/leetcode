d68163dcddba7ad8c672741350fbc78869076e24
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-05 11:46:57 +0530

    Time: 17 ms (52.53%), Space: 11.7 MB (94.05%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def subsets(self, nums):
-        res  = []
-        sol = []
-        def dfs(i):
-            if(len(nums)) <= i:
-                res.append(sol[:])
-                return
-            sol.append(nums[i])
-            dfs(i+1)
-            sol.pop()
-            dfs(i+1)
-        dfs(0)
-        return(res)
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result=[[]]
+        for num in nums:
+            result+=[i+[num] for i in result]
+        return result
+        
