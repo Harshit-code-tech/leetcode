191ef0780fe09fb52365d71747ff06093b7141eb
Harshit Ghosh <harshitghosh6@gmail.com>
2024-12-29 18:24:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,9 +1,16 @@
-import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger one = new BigInteger(a, 2);
-        BigInteger two = new BigInteger(b, 2);
-        BigInteger sum = one.add(two);
-        return sum.toString(2);
+        StringBuilder sb = new StringBuilder();
+        int i= a.length()-1,j=b.length()-1,c=0;
+        while(i>=0||j>=0||c!=0){
+            int sum = c;
+            if(i>=0)sum +=a.charAt(i--)-'0';
+            if(j>=0)sum+=b.charAt(j--)-'0';
+            result.append(sum%2);
+            carry=sum/2;
+        }
+        return result.reverse().toString();
+
+
     }
 }
