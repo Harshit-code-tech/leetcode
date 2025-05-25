0071b8e7fd430ce6929b6435eeacca1da968d74c
Harshit Ghosh <harshitghosh6@gmail.com>
2025-05-25 12:33:56 +0530

    Time: 0 ms (100%), Space: 18 MB (50.08%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,31 +1,32 @@
 class Solution:
-    def is_sorted(self, nums):
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                return False
-        return True
+    # def is_sorted(self, nums):
+    #     for i in range(len(nums) - 1):
+    #         if nums[i] > nums[i + 1]:
+    #             return False
+    #     return True
     
-    def rotate_left(self, nums):
-        if len(nums) == 0:
-            return nums
-        first = nums[0]
-        for i in range(1, len(nums)):
-            nums[i - 1] = nums[i]
-        nums[-1] = first
-        return nums
-    def make_sorted(self, nums):
-        for _ in range(len(nums)):
-            if self.is_sorted(nums):
-                return nums
-            nums = self.rotate_left(nums)
-        return nums
+    # def rotate_left(self, nums):
+    #     if len(nums) == 0:
+    #         return nums
+    #     first = nums[0]
+    #     for i in range(1, len(nums)):
+    #         nums[i - 1] = nums[i]
+    #     nums[-1] = first
+    #     return nums
+    # def make_sorted(self, nums):
+    #     for _ in range(len(nums)):
+    #         if self.is_sorted(nums):
+    #             return nums
+    #         nums = self.rotate_left(nums)
+    #     return nums
 
     
     def findMin(self, nums: List[int]) -> int:
-        nums = self.make_sorted(nums)
+        # nums = self.make_sorted(nums)
 
-        left = 0
-        right = len(nums) - 1
+        # left = 0
+        # right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left < right:
             mid = (left + right) // 2
             if nums[mid] > nums[right]:
